01c2feac4b97969ed7355851b5ea5f915510ec08
mona95 <shamona9594@gmail.com>
2021-10-16 15:56:12 +0300

    [#113]-rework: generated a new utils for linkify

--- client/src/components/Card.vue
+++ client/src/components/Card.vue
@@ -268,6 +268,7 @@
 
 <script>
 import service from "@/service";
+import { linkify } from "@/utils/linkify.js";
 import { mapActions, mapGetters } from "vuex";
 
 export default {
@@ -290,7 +291,7 @@ export default {
   },
   mounted() {
     // insert clickable content for posts
-    document.getElementById(`post-${this.post.id}`).innerHTML =  this.convertLinks(this.post.message)
+    document.getElementById(`post-${this.post.id}`).innerHTML =  linkify(this.post.message)
   },
   computed: {
     ...mapGetters(["currUser"]),
@@ -394,52 +395,6 @@ export default {
     setTimeFormat(date) {
       let dateObject = new Date(date);
       return dateObject.toLocaleTimeString();
-    },
-    convertLinks: (input) => {
-      let text = input;
-      const linksFound = text.match(/(?:www|https?)[^\s]+/g);
-      const aLink = [];
-
-      if (linksFound != null) {
-        for (let i = 0; i < linksFound.length; i++) {
-          let replace = linksFound[i];
-          if (!linksFound[i].match(/(http(s?)):\/\//)) {
-            replace = "http://" + linksFound[i];
-          }
-          let linkText = replace.split("/")[2];
-          if (linkText.substring(0, 3) == "www") {
-            linkText = linkText.replace("www.", "");
-          }
-          if (linkText.match(/youtu/)) {
-            let youtubeID = replace.split("/").slice(-1)[0];
-            aLink.push(
-              '<div class="video-wrapper"><iframe src="https://www.youtube.com/embed/' +
-                youtubeID +
-                '" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>'
-            );
-          } else if (linkText.match(/vimeo/)) {
-            let vimeoID = replace.split("/").slice(-1)[0];
-            aLink.push(
-              '<div class="video-wrapper"><iframe src="https://player.vimeo.com/video/' +
-                vimeoID +
-                '" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe></div>'
-            );
-          } else {
-            aLink.push(
-              '<a style="color:black;" href="' + replace + '" target="_blank">' + linkText + "</a>"
-            );
-          }
-          text = text
-            .split(linksFound[i])
-            .map((item) => {
-              return aLink[i].includes("iframe") ? item.trim() : item;
-            })
-            .join(aLink[i]);
-        }
-        return text;
-      } else {
-        return input;
-      }
     },
   },
 };

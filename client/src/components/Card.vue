8cfd100836c1d2477ab388a6e3528cd62d2d1621
mona95 <shamona9594@gmail.com>
2021-10-16 15:51:51 +0300

    [#113]-feat: first attempt to make the links clickable in the post message

--- client/src/components/Card.vue
+++ client/src/components/Card.vue
@@ -37,8 +37,7 @@
       </div>
       <div class="techcard-content">
         <div class="techcard-content-title">{{ post.title }}</div>
-        <div class="techcard-content-text">
-          {{ post.message }}
+        <div :id="`post-${post.id}`" :refs="`post-${post.id}`" class="techcard-content-text">
         </div>
       </div>
       <div class="techcard-footer">
@@ -289,6 +288,10 @@ export default {
       commentsLoaded: false,
     };
   },
+  mounted() {
+    // insert clickable content for posts
+    document.getElementById(`post-${this.post.id}`).innerHTML =  this.convertLinks(this.post.message)
+  },
   computed: {
     ...mapGetters(["currUser"]),
   },
@@ -391,6 +394,52 @@ export default {
     setTimeFormat(date) {
       let dateObject = new Date(date);
       return dateObject.toLocaleTimeString();
+    },
+    convertLinks: (input) => {
+      let text = input;
+      const linksFound = text.match(/(?:www|https?)[^\s]+/g);
+      const aLink = [];
+
+      if (linksFound != null) {
+        for (let i = 0; i < linksFound.length; i++) {
+          let replace = linksFound[i];
+          if (!linksFound[i].match(/(http(s?)):\/\//)) {
+            replace = "http://" + linksFound[i];
+          }
+          let linkText = replace.split("/")[2];
+          if (linkText.substring(0, 3) == "www") {
+            linkText = linkText.replace("www.", "");
+          }
+          if (linkText.match(/youtu/)) {
+            let youtubeID = replace.split("/").slice(-1)[0];
+            aLink.push(
+              '<div class="video-wrapper"><iframe src="https://www.youtube.com/embed/' +
+                youtubeID +
+                '" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>'
+            );
+          } else if (linkText.match(/vimeo/)) {
+            let vimeoID = replace.split("/").slice(-1)[0];
+            aLink.push(
+              '<div class="video-wrapper"><iframe src="https://player.vimeo.com/video/' +
+                vimeoID +
+                '" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe></div>'
+            );
+          } else {
+            aLink.push(
+              '<a style="color:black;" href="' + replace + '" target="_blank">' + linkText + "</a>"
+            );
+          }
+          text = text
+            .split(linksFound[i])
+            .map((item) => {
+              return aLink[i].includes("iframe") ? item.trim() : item;
+            })
+            .join(aLink[i]);
+        }
+        return text;
+      } else {
+        return input;
+      }
     },
   },
 };
@@ -511,7 +560,7 @@ body {
   font-weight: bold;
 }
 
-.techcard-content-text {
+.link {
   margin-top: 6px;
 }
 
